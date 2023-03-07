b91f1639992f2bf2ce486639637aaae065133c30
Noel Brown <noelbrowndboss@gmail.com>
2023-03-07 12:23:56 -0800

    omit two letters

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for a in range(ord('a'), ord('z')+1):
     if (a != ord('q')) and (a != ord('e')):
-            print('{}'.format(chr(a)), end="")
+        print('{}'.format(chr(a)), end="")
