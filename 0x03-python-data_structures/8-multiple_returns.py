3d4ef1ed9b6115ba309dd12105fafc3e8a74aff2
Noel Brown <noelbrowndboss@gmail.com>
2023-03-13 15:43:35 -0700

    return first letter

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = sentence[0]
+    if length == 0:
+        first_char = "None"
+    return (length, first_char)
