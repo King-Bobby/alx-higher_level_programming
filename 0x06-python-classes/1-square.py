a585deeb0e13753ef169782adbdc38c9f41c52f2
Noel Brown <noelbrowndboss@gmail.com>
2023-03-29 15:54:22 -0700

    making a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines class Square with a private attribute size
+"""
+
+
+class Square:
+    def __init__(self, size):
+        """
+        Initializes Class Square
+
+        Attributes:
+            size: size of a side of a square
+        """
+
+        self.__size = size
