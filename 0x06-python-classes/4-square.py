10bb7f83e6a10eb8a533a4ac2a1d4beeaa2152ac
Noel Brown <noelbrowndboss@gmail.com>
2023-03-29 17:20:45 -0700

    using property

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Defines class Square with private attribute size and initializes it
+"""
+
+
+class Square:
+    """
+    Defines Class square
+
+    Functions:
+        __init__(self, size)
+        area(self)
+        size(self)
+        size(self, value)
+    """
+
+    def __init__(self, size=0):
+        """
+        Initalizes Square
+
+        Attributes:
+            size(int): size of a side of a square, default value is 0
+        """
+
+        self.__size = size
+
+    def area(self):
+        """
+        Public method area: finds the area of a square using size as its sides
+        """
+
+        Sq_area = self.__size * self.__size
+        return Sq_area
+
+    @property
+    def size(self):
+        """Getter function: To return the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Setter function: To set size to value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
