a92189b7466725d46aa82717a9f16d8b41a12138
Noel Brown <noelbrowndboss@gmail.com>
2023-04-04 18:44:02 -0700

    print name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 3-say_my_name
+contains one method that return first name and last name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """prints a string showing the first name and last name"""
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
