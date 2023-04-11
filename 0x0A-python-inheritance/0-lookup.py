35086d6a7622899fe3fa4ed09cd836034b0f32ac
Noel Brown <noelbrowndboss@gmail.com>
2023-04-11 16:51:09 -0700

    return list of methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module- 0-lookup that contains method lookup
+"""
+
+
+def lookup(obj):
+    """returns the list of available attributes and methods in obj"""
+    return dir(obj)
