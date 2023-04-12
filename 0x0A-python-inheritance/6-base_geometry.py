c41be13262145ae68dcbfb57457113c35d1815c4
Noel Brown <noelbrowndboss@gmail.com>
2023-04-11 17:39:55 -0700

    Rasie exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry that contains class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """class that contains public instance method area"""
+    def area(self):
+        """a method that raises an exception"""
+        raise Exception("area() is not implemented")
