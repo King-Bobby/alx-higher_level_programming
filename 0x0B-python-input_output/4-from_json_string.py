5d14ed6f45961219b551c74c227f0acd37a139ba
Noel Brown <noelbrowndboss@gmail.com>
2023-04-12 19:00:30 -0700

    json.loads

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 4-from_json_string containing function from_json_string
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """returns an object(Python data structure) represented by a JSON string"""
+    return json.loads(my_str)
