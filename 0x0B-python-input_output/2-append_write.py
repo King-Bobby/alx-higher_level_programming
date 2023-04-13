eef07d715dbee2101c0a804fdd8f20e1d9c050fd
Noel Brown <noelbrowndboss@gmail.com>
2023-04-12 18:44:40 -0700

    append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,6 @@ Module 2-append_write that contains function append_write
 """
 
 
-
 def append_write(filename="", text=""):
     """appends text to file in filename"""
     with open(filename, 'a') as f:
