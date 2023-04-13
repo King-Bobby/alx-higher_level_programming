b9b3142afa1dc97338e4c2dbc38a407f9e5efcb3
Noel Brown <noelbrowndboss@gmail.com>
2023-04-12 18:43:29 -0700

    append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 2-append_write that contains function append_write
+"""
+
+
+
+def append_write(filename="", text=""):
+    """appends text to file in filename"""
+    with open(filename, 'a') as f:
+        return(f.write(text))
