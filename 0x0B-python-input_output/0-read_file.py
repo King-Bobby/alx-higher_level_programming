53a80b44cc375548ede7f2d8b1053df880c6174a
Noel Brown <noelbrowndboss@gmail.com>
2023-04-12 18:24:28 -0700

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Module 0-read_write that contains function read_file
+"""
+
+
 def read_file(filename=""):
+    """reads a file and prints it to stdout"""
     with open(filename) as f:
         for line in f:
             print(line, end="")
