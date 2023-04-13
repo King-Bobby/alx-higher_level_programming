aff7de1037173d331f5edcde98a7f879720b53e5
Noel Brown <noelbrowndboss@gmail.com>
2023-04-13 09:59:42 -0700

    attrs

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/pythonu3
+"""
+Module 10-student that contains class Student
+"""
+
+
+class Student:
+    """Class Student that contains function to_json"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes class Student
+        Public attributes:
+            first_name
+            last_name
+            age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance or
+        only attribute names contained in the list attrs
+        Arg:
+            attrs - is a list of strings"""
+        _dict = {}
+        if attrs is None:
+            return self.__dict__
+        else:
+            for i in attrs:
+                if i in self.__dict__.keys():
+                    _dict[i] = self.__dict__[i]
+            return _dict
