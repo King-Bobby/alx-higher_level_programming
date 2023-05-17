b846b65f9dc2400913bd3a9bb2bd414751cc7c66
Noel Brown <noelbrowndboss@gmail.com>
2023-05-16 18:33:56 -0700

    select

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 --  lists all records of the table second_table
 -- Dont list rows without a name value
 
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name != '' AND name IS NOT NULL ORDER BY score DESC;
