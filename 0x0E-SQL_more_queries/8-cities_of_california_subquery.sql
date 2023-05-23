f46f4b85ff98f84287707962726e1f86750b37e4
Noel Brown <noelbrowndboss@gmail.com>
2023-05-23 06:20:42 -0700

    ...

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,4 +4,5 @@
 
 SELECT id, name
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = "California") GROUP BY id ORDER BY id ASC;
+WHERE state_id = (SELECT id FROM states WHERE name = "California") 
+GROUP BY id ORDER BY id ASC;
