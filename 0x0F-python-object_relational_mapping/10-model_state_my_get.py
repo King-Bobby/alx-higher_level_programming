f63911a26c35d32e544dfeb824731a317d7fef11
Noel Brown <noelbrowndboss@gmail.com>
2023-06-19 07:18:32 -0700

    updates

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects that contain the letter a
+Script that prints the State object with the name passed as an argument
 from the database hbtn_0e_6_usa
 """
 
@@ -9,9 +9,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     # Assign values to variables
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    username, password, = sys.argv[1], sys.argv[2]
+    database, state_name = sys.argv[3], sys.argv[4]
 
     # Create the connection string
     connection_string = f"mysql+mysqldb://" + username + ":" + \
@@ -26,15 +28,13 @@ if __name__ == "__main__":
     # Create a session
     session = Session()
 
-    # Query the State objects containing the letter 'a'
-    query = session.query(State)
-    states = query.filter(State.name.like('%a%')).order_by(State.id).all()
+    # Query the State object with the given name
+    state = session.query(State).filter(State.name == state_name).first()
 
-    if not states:
-        print("Nothing")
+    if state is None:
+        print("Not found")
     else:
-        for state in states:
-            print(f"{state.id}: {state.name}")
+        print(state.id)
 
     # Close the session
     session.close()
