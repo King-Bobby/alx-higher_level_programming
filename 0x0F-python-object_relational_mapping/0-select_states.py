5cda8d5f0728c31bb056b6f763346ccdddeb9499
Noel Brown <noelbrowndboss@gmail.com>
2023-06-19 05:32:36 -0700

    added documentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+a script that lists all states from the database hbtn_0e_0_usa:
+"""
+
+
 import MySQLdb
 import sys
 
---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Get the MySQL connection information from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create the connection string
+    connection_string = f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
+
+    # Create the SQLAlchemy engine
+    engine = create_engine(connection_string)
+
+    # Bind the engine to the base class
+    Base.metadata.bind = engine
+
+    # Create a session factory
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    # Retrieve all State objects and sort them by ID in ascending order
+    states = session.query(State).order_by(State.id).all()
+
+    # Display the State objects
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    # Close the session
+    session.close()
