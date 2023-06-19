c1245d494c6808bfb064a79119ba4882b0acc02e
Noel Brown <noelbrowndboss@gmail.com>
2023-06-19 07:00:28 -0700

    first()

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,18 +3,15 @@
 Script that prints the first State object from the database hbtn_0e_6_usa
 """
 
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
-    # Check if all arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: ./7-model_state_fetch_first.py \
-                <username> <password> <database>")
-        sys.exit(1)
 
+if __name__ == "__main__":
+    # Assign values to variables
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
     # Create the connection string
