4ce09dbe08bfb8ba1238d1b32ec8d2c800b9fae3
Noel Brown <noelbrowndboss@gmail.com>
2023-06-22 16:30:33 -0700

    states with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
             db=sys.argv[3]
             )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states
+                   WHERE name REGEXP '^[N]' ORDER BY id ASC")
     results = cursor.fetchall()
     for row in results:
         print(row)
