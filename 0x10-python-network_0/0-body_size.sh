894ba103366f871edb75a23b7f2b8cd0b9b44dee
King-Bobby <noelbrowndboss@gmail.com>
2023-06-30 12:19:33 -0700

    Content length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-
+# Script takes URL, sends request and displays Content length of response
 curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
