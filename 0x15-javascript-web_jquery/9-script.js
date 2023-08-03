b2ba0197c0e379b9aeb2f3b74e5df706fa6ba7fb
King-Bobby <noelbrowndboss@gmail.com>
2023-08-02 18:52:26 -0700

    README

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+$(function() {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function(response, status) {
+    if (status === 'success') {
+      $('#hello').text(response.hello);
+    };
+  });
+});
