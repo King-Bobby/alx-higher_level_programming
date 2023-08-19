8e52c7f4288e7e76b3231828d719a1bfb9420f25
King-Bobby <noelbriwndboss@gmail.com>
2023-08-19 15:53:56 -0700

    testing 3

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains a function that splits a text into sentences
+"""
+
+
+def text_indentation(text):
+    """
+    prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in ".?:" or text[i] == "\n":
+            print("\n")
+            i += 1
+        i += 1
