e80b75d86dc6969c33c59e562d1cd772746db67a
King-Bobby <noelbriwndboss@gmail.com>
2023-08-19 15:14:13 -0700

    testing 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,6 +12,6 @@ def say_my_name(first_name, last_name=""):
     elif type(last_name) is not str:
         raise TypeError("last_name must be a string")
     if last_name == "":
-        print(f"My name is {first_name}")
+        print(f"My name is {first_name} ")
     else:
         print(f"My name is {first_name} {last_name}")
