624cc705ad8dfb1e6b1766a626168085e42c6b17
King-Bobby <noelbriwndboss@gmail.com>
2023-08-20 15:59:48 -0700

    testing 7

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,44 +1,46 @@
 #!/usr/bin/python3
 """Unittest for max_integer([..])
 """
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def max_at_end(self):
+    """Define unittests for max_integer([..])."""
+    def test_max_at_end(self):
         """tests if max value is at the end"""
         llist = [1, 2, 3, 4]
         self.assertEqual(max_integer(llist), 4)
 
-    def max_at_beginning(self):
+    def test_max_at_beginning(self):
         """tests if max value is at the beginning"""
         llist = [4, 3, 2, 1]
         self.assertEqual(max_integer(llist), 4)
 
-    def max_in_middle(self):
+    def test_max_in_middle(self):
         """tests if max value is in the middle"""
         llist = [1, 2, 5, 3, 4]
         self.assertEqual(max_integer(llist), 5)
 
-    def one_negative_number(self):
+    def test_one_negative_number(self):
         """tests if it finds max even if one negative number exists"""
         llist = [1, -2, 3, 4, -5]
         self.assertEqual(max_integer(llist), 4)
 
-    def negative_numbers(self):
+    def test_negative_numbers(self):
         """
         tests if it finds max even if the list is only negative numbers
         """
         llist = [-1, -2, -3, -4, -5]
         self.assertEqual(max_integer(llist), -1)
 
-    def one_element(self):
+    def test_one_element(self):
         """tests if list has only one element"""
         llist = [2]
         self.assertEqual(max_integer(llist), 2)
 
-    def empty_list(self):
+    def test_empty_list(self):
         """tests if the list is empty"""
         llist = []
         self.assertEqual(max_integer(llist), None)
